21514afe80e98cc2362cd4f05079db55f30bf0b0
Genevieve <genevievepc47@gmail.com>
2020-02-06 09:27:29 -0500

    added a button and changed the name

--- app/src/main/java/edu/temple/signupform/FormActivity.java
+++ app/src/main/java/edu/temple/signupform/FormActivity.java
@@ -3,7 +3,7 @@ package edu.temple.signupform;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class FormActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
